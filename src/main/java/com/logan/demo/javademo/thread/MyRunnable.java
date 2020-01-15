5d1ca3347b09f8044e179a36233a31c7623aa1bc
changzheng <changzheng@mafengwo.com>
2020-01-15 10:14:32 +0800

    线程

---
+++ src/main/java/com/logan/demo/javademo/thread/MyRunnable.java
@@ -0,0 +1,22 @@
+package com.logan.demo.javademo.thread;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * Created 2020/1/8. 10:28 上午
+ * 实现java.lang.Runnable接口
+ * 具体做法：实现Runnable接口，并重写该接口的run()方法，该run()方法同样是线程执行体，创建Runnable实现类的实例，并以此实例作为Thread类的target来创建Thread对象，该Thread对象才是真正的线程对象。
+ * @author changzheng
+ */
+@AllArgsConstructor
+public class MyRunnable implements Runnable{
+
+    private String name;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + "运行  ：" + i);
+        }
+    }
+}
